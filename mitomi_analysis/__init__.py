21f64d81c2b46f36b7cf76b0f42052162aee291b
Tyler Shimko <TShimko126@gmail.com>
2016-02-15 14:56:42 -0800

    Update imports

--- mitomi_analysis/__init__.py
+++ mitomi_analysis/__init__.py
@@ -1,6 +1,6 @@
-import chipAnalysis_v5
-import gprFilesToConcatFile_v3
-import processConcat_PR8_v3
-import scatterplotRep1vsRep2_v5
-import outputDataForFReduce_NewConcatFiles
-import runMultiFREDUCE
+from . import chipAnalysis_v5
+from . import gprFilesToConcatFile_v3
+from . import processConcat_PR8_v3
+from . import scatterPlotRep1vsRep2_v5
+from . import outputDataForfREDUCE_newConcatFiles
+from . import runMultiFREDUCE
